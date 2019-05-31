141d21f62d30fcf5fe6baa2f237fa94ed719cea0
Qiumu <sddz_ly@foxmail.com>
2019-05-31 16:30:28 +0800

    添加类别选择和时间选择

--- app/src/main/java/com/demo/awesomeledger/activity/AddItemActivity.java
+++ app/src/main/java/com/demo/awesomeledger/activity/AddItemActivity.java
@@ -5,9 +5,31 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.SimpleAdapter;
+import android.widget.AdapterView;
+import android.widget.DatePicker;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+import java.util.Calendar;
 import com.demo.awesomeledger.R;
 
-public class AddItemActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddItemActivity extends AppCompatActivity implements View.OnClickListener, DatePicker.OnDateChangedListener{
+    private Spinner spinner;
+    private List<String> kind_list;
+    private ArrayAdapter<String> arr_adapter;
+    private TextView tvDate;
+    private int year, month, day;
+    private StringBuffer date;
+    private Context context;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,7 +44,91 @@ public class AddItemActivity extends AppCompatActivity {
             actionBar.setHomeAsUpIndicator(R.mipmap.ic_return_64);
         }
 
+        date = new StringBuffer();
+        context = this;
+        initView();
+        initDateTime();
     }
+
+    //初始化控件
+    private void initView(){
+        tvDate = (TextView) findViewById(R.id.dateView);
+        tvDate.setOnClickListener(this);
+
+        //类别选择部分
+        spinner = (Spinner) findViewById(R.id.kind);
+        kind_list = new ArrayList<String>();
+        kind_list.add("支出");
+        kind_list.add("收入");
+        //适配器
+        arr_adapter= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,kind_list);
+        //设置样式
+        arr_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //加载适配器
+        spinner.setAdapter(arr_adapter);
+        //绑定事件监听
+        spinner.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {//选择item的选择点击监听事件
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int position, long arg3) {
+                // TODO Auto-generated method stub
+            }
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+        });
+    }
+    /**
+     * 获取当前的日期和时间
+     */
+    private void initDateTime() {
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        tvDate.setText(date.append(String.valueOf(month+1)).append("月").append(day).append("日▼"));
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.dateView:
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setPositiveButton("设置", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (date.length() > 0) { //清除上次记录的日期
+                            date.delete(0, date.length());
+                        }
+                        tvDate.setText(date.append(String.valueOf(month+1)).append("月").append(day).append("日▼"));
+                        dialog.dismiss();
+                    }
+                });
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                final AlertDialog dialog = builder.create();
+                View dialogView = View.inflate(context, R.layout.dialog_date, null);
+                final DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.datePicker);
+
+                dialog.setTitle("设置日期");
+                dialog.setView(dialogView);
+                dialog.show();
+                //初始化日期监听事件
+                datePicker.init(year, month , day, this);
+                break;
+        }
+    }
+    @Override
+    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        this.year = year;
+        this.month = monthOfYear;
+        this.day = dayOfMonth;
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -35,4 +141,6 @@ public class AddItemActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
+
 }

ea32776e03578dd53fb40eff0c55b738283ec9d5
Lee <lixiangyuan1998@outlook.com>
2019-06-20 18:05:31 +0800

    修改数据库结构和Dao 更新gitignore

--- app/src/main/java/com/demo/awesomeledger/dao/BaseDao.java
+++ app/src/main/java/com/demo/awesomeledger/dao/BaseDao.java
@@ -5,7 +5,7 @@ import java.util.List;
 abstract class BaseDao<T> {
 
     public abstract List<T> query(boolean distinct, String selection, String[] selectionArgs,
-                                  String groupBy, String having, String orderBy);
+                                  String groupBy, String having, String orderBy, boolean deleted);
 
     public abstract long insert(T t);
 
--- app/src/main/java/com/demo/awesomeledger/dao/ItemDao.java
+++ app/src/main/java/com/demo/awesomeledger/dao/ItemDao.java
@@ -40,24 +40,42 @@ public class ItemDao extends BaseDao<Item> {
 
     private ContentValues getContentValues(@NonNull Item item) {
         ContentValues values = new ContentValues();
+        values.put("uuid", item.getUuid());
         values.put("date", item.getDate().getTime());
         values.put("type", item.getItemType().name());
         values.put("kind", item.getItemKind().name());
         values.put("address", item.getAddress());
         values.put("money", item.getMoney());
         values.put("comment", item.getComment());
+        values.put("created_at", item.getCreatedAt().getTime());
+        values.put("updated_at", item.getUpdatedAt().getTime());
+        if (item.getDeletedAt() != null) {
+            values.put("deleted_at", item.getDeletedAt().getTime());
+        }
         return values;
     }
 
-    private Item createItemFromCursor(Cursor cursor) {
-        return new Item(cursor.getInt(0), new Date(cursor.getLong(1)),
-                ItemType.valueOf(cursor.getString(2)),
-                ItemKind.valueOf(cursor.getString(3)),
-                cursor.getString(4),
-                cursor.getDouble(5),
-                cursor.getString(6));
+    private Item createItemFromCursor(Cursor cursor, boolean deleted) {
+        if (!deleted && cursor.getLong(10) != 0) {
+            return null;
+        }
+        Date deletedAt = null;
+        if (deleted && cursor.getLong(10) != 0) {
+            deletedAt = new Date(cursor.getLong(10));
+        }
+        return new Item(cursor.getInt(0), cursor.getString(1),
+                new Date(cursor.getLong(2)),
+                ItemType.valueOf(cursor.getString(3)),
+                ItemKind.valueOf(cursor.getString(4)),
+                cursor.getString(5),
+                cursor.getDouble(6),
+                cursor.getString(7),
+                new Date(cursor.getLong(8)),
+                new Date(cursor.getLong(9)),
+                deletedAt);
     }
 
+    // 获取指定id的item, 软删除的项目会被忽略
     @Nullable
     public Item get(long id) {
         SQLiteDatabase db = itemHelper.getReadableDatabase();
@@ -65,11 +83,26 @@ public class ItemDao extends BaseDao<Item> {
         args[0] = String.valueOf(id);
         Cursor cursor = db.query(true, ItemHelper.name, null, "id=?",
                 args, null, null, null, null);
-        Item item;
+        Item item = null;
         if (cursor.moveToFirst()) {
-            item = createItemFromCursor(cursor);
-        } else {
-            item = null;
+            item = createItemFromCursor(cursor, false);
+        }
+        cursor.close();
+        db.close();
+        return item;
+    }
+
+    // 获取指定uuid的item, 软删除的item也会被返回
+    @Nullable
+    public Item get(String uuid) {
+        SQLiteDatabase db = itemHelper.getReadableDatabase();
+        String[] args = new String[1];
+        args[0] = uuid;
+        Cursor cursor = db.query(true, ItemHelper.name, null, "uuid=?",
+                args, null, null, null, null);
+        Item item = null;
+        if (cursor.moveToFirst()) {
+            item = createItemFromCursor(cursor, true);
         }
         cursor.close();
         db.close();
@@ -98,24 +131,33 @@ public class ItemDao extends BaseDao<Item> {
         return args;
     }
 
-    public List<Item> getItems(Calendar month) {
-        return query(true, "date>=? AND date<=?", getDateArgs(month), null, null, "-date");
+    // 获得所有item, 包含被软删除的item
+    @Nullable
+    public List<Item> getItems() {
+        return query(true, null, null, null, null, "-date", true);
     }
 
+    @Nullable
+    public List<Item> getItems(Calendar month) {
+        return query(true, "date>=? AND date<=?", getDateArgs(month), null, null, "-date", false);
+    }
+
+    @Nullable
     public List<Item> getItems(Calendar month, ItemType type) {
         return query(true, "date>=? AND date<=? AND type=?", getDateArgs(month, type),
-                null, null, "-date");
+                null, null, "-date", false);
     }
 
+    @Nullable
     public List<Item> getItems(Calendar month, ItemType type, ItemKind kind) {
         return query(true, "date>=? AND date<=? AND type=? AND kind=\"" + kind.name() + "\"",
-                getDateArgs(month, type), null, null, null);
+                getDateArgs(month, type), null, null, null, false);
     }
 
     @Override
     @Nullable
     public List<Item> query(boolean distinct, String selection, String[] selectionArgs,
-                            String groupBy, String having, String orderBy) {
+                            String groupBy, String having, String orderBy, boolean deleted) {
         SQLiteDatabase db = itemHelper.getReadableDatabase();
         Cursor cursor = db.query(distinct, ItemHelper.name, null, selection, selectionArgs,
                 groupBy, having, orderBy, null);
@@ -123,8 +165,10 @@ public class ItemDao extends BaseDao<Item> {
         if (cursor.moveToFirst()) {
             queryResults =  new ArrayList<>(cursor.getCount());
             do {
-                Item item = createItemFromCursor(cursor);
-                queryResults.add(item);
+                Item item = createItemFromCursor(cursor, deleted);
+                if (item != null) {
+                    queryResults.add(item);
+                }
             } while (cursor.moveToNext());
         }
         cursor.close();
@@ -145,16 +189,24 @@ public class ItemDao extends BaseDao<Item> {
         SQLiteDatabase db = itemHelper.getWritableDatabase();
         String[] whereArgs = new String[1];
         whereArgs[0] = String.valueOf(item.getId());
+        item.setUpdatedAt(new Date());
         db.update(ItemHelper.name, getContentValues(item), "id=?", whereArgs);
         db.close();
     }
 
+    // 软删除
     @Override
     public void delete(@NonNull Item item) {
+        item.setDeletedAt(new Date());
+        update(item);
+    }
+
+    // 彻底删除item
+    public void delete(@NonNull String uuid) {
         SQLiteDatabase db = itemHelper.getWritableDatabase();
         String[] whereArgs = new String[1];
-        whereArgs[0] = String.valueOf(item.getId());
-        db.delete(ItemHelper.name, "id=?", whereArgs);
+        whereArgs[0] = uuid;
+        db.delete(ItemHelper.name, "uuid=?", whereArgs);
         db.close();
     }
 }

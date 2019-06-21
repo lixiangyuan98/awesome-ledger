b73478b3b4ea91a9fb2627c5a8893bb8860e857e
Lee <lixiangyuan1998@outlook.com>
2019-06-22 01:43:56 +0800

    修复了另一台手机无法同步删除的bug

---
+++ app/src/main/java/com/demo/awesomeledger/gson/InsertResponse.java
@@ -0,0 +1,16 @@
+package com.demo.awesomeledger.gson;
+
+import java.util.List;
+
+public class InsertResponse {
+
+    private List<String> localDelete;
+
+    public List<String> getLocalDelete() {
+        return localDelete;
+    }
+
+    public void setLocalDelete(List<String> localDelete) {
+        this.localDelete = localDelete;
+    }
+}
